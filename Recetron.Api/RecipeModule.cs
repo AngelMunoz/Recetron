54b0e93569a544de426f98350caf99d0f72758e2
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2020-06-27 23:23:19 -0600

    add: - get by id - delete by id - update recipe

--- Recetron.Api/RecipeModule.cs
+++ Recetron.Api/RecipeModule.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Carter;
 using Carter.ModelBinding;
+using Carter.Request;
 using Carter.Response;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Primitives;
 using MongoDB.Bson;
 using Recetron.Api.Interfaces;
 using Recetron.Core.Models;
@@ -33,6 +29,27 @@ namespace Recetron.Api
         var recipes = await _recipes.FindByUser(user.Id ?? ObjectId.Empty, page, limit);
         await res.Negotiate(recipes);
         return;
+      });
+      
+      Get("{id:string}", async (req, res) =>
+      {
+        var strId = req.RouteValues.As<string>("id");
+        var recipeId = ObjectId.Parse(strId);
+        var recipe = await _recipes.FindOne(recipeId);
+        var user = await _auth.ExtractUserAsync(ModuleHelpers.ExtractTokenStr(req.HttpContext));
+        if (recipe.UserId != user?.Id)
+        {
+          res.StatusCode = 403;
+          await res.Negotiate(
+            new ErrorResponse
+            {
+              Message = "You don't have access to this recipe",
+            }
+          );
+          return;
+        }
+
+        await res.Negotiate(recipe);
       });
 
       Post("", async (req, res) =>
@@ -66,6 +83,94 @@ namespace Recetron.Api
 
         res.StatusCode = 201;
         await res.Negotiate(recipe);
+      });
+
+      Put("", async (req, res) =>
+      {
+        var user = await _auth.ExtractUserAsync(ModuleHelpers.ExtractTokenStr(req.HttpContext));
+        if (user?.Id == null)
+        {
+          res.StatusCode = 422;
+          await res.Negotiate(new ErrorResponse { Message = "Missing User from Token" });
+          return;
+        }
+
+        var result =  await req.BindAndValidate<Recipe>();
+        if (!result.ValidationResult.IsValid)
+        {
+          res.StatusCode = 400;
+          await res.Negotiate(
+            new ErrorResponse
+            {
+              Message = "Failed Validation",
+              Errors = result.ValidationResult.GetFormattedErrors()
+            }
+          );
+          return;
+        }
+
+        var recipe = result.Data;
+
+        if (recipe.UserId != user.Id)
+        {
+          res.StatusCode = 403;
+          await res.Negotiate(
+            new ErrorResponse
+            {
+              Message = "You don't have access to this recipe",
+            }
+          );
+          return;
+        }
+  
+        var didUpdate = await _recipes.Update(recipe);
+        if (!didUpdate)
+        {
+          res.StatusCode = 422;
+          await res.Negotiate(
+            new ErrorResponse
+            {
+              Message = "Could Update Recipe",
+            }
+          );
+          return;
+        }
+
+        await res.Negotiate(recipe);
+      });
+
+      Delete("{id:string}", async (req, res) =>
+      {
+        var strId = req.RouteValues.As<string>("id");
+        var recipeId = ObjectId.Parse(strId);
+        var recipe = await _recipes.FindOne(recipeId);
+        var user = await _auth.ExtractUserAsync(ModuleHelpers.ExtractTokenStr(req.HttpContext));
+        if (recipe.UserId != user?.Id)
+        {
+          res.StatusCode = 403;
+          await res.Negotiate(
+            new ErrorResponse
+            {
+              Message = "You don't have access to this recipe",
+            }
+          );
+          return;
+        }
+
+        var didDestroy = await _recipes.Destroy(recipeId);
+        if (!didDestroy)
+        {
+          res.StatusCode = 422;
+          await res.Negotiate(
+            new ErrorResponse
+            {
+              Message = "Failed To Delete This Recipe",
+            }
+          );
+          return;
+        }
+
+        res.StatusCode = 204;
       });
     }
   }
