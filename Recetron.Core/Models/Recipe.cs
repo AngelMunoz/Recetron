1d8302c0c5e9c0291db618f04eb49b53c47d5742
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2020-06-30 19:57:26 -0600

    add validations for blazor

--- Recetron.Core/Models/Recipe.cs
+++ Recetron.Core/Models/Recipe.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.IdGenerators;
 
@@ -13,12 +13,15 @@ namespace Recetron.Core.Models
 
     public string? UserId { get; set; }
 
+    [Required, MinLength(4)]
     public string? Title { get; set; }
 
     public string? ImageUrl { get; set; }
 
+    [MaxLength(500)]
     public string? Description { get; set; }
 
+    [MaxLength(240)]
     public string? Notes { get; set; }
 
     public IEnumerable<Ingredient> Ingredients { get; set; } = new Ingredient[] { };
@@ -30,6 +33,8 @@ namespace Recetron.Core.Models
   public class RecipeStep
   {
     public int? Order { get; set; }
+
+    [Required, MaxLength(300)]
     public string? Directions { get; set; }
     public string? ImageUrl { get; set; }
 
@@ -37,12 +42,15 @@ namespace Recetron.Core.Models
 
   public class Ingredient
   {
+    [Required, MaxLength(100)]
     public string? Name { get; set; }
 
     /// meaning how many/much of the measurment unit
+    [Required, MaxLength(10)]
     public string? Amount { get; set; }
 
     /// what is the measurment unit? spoons, grams, ounces, 
+    [Required, MaxLength(10)]
     public string? Unit { get; set; }
 
     public IEnumerable<Ingredient> Replacements { get; set; } = new Ingredient[] { };
